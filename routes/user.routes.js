60be13a7df9ac7f8ab8768ad78ca5e01ebc971f8
Yash-Vashishth <imyashvashishth05@gmail.com>
2025-11-02 17:48:09 +0530

    y

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,4 +4,4 @@ const controller = require("../controllers/user.controller");
 router.get("/api/students", controller.getAllUsers);
 router.get("/api/students/:id", controller.getUser);
 router.post("/api/students", controller.createUser);
-router.put("/api/students/:id", controller.updateUser);
+router.put("/api/students/:id/enroll", controller.updateUser);
